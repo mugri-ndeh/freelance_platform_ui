fe219b4cc513c141e7b463a289b1619cee6026b7
mugri-ndeh <frunwindeh@yahoo.com>
2022-08-03 17:49:12 +0100

    screen util added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:freelance_platform_ui/core/themes/theme.dart';
 import 'package:freelance_platform_ui/presentation/screens/onbaording/welcome.dart';
 import 'package:provider/provider.dart';
@@ -13,10 +14,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: customLightTheme(),
-      home: Welcome(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: customLightTheme(),
+        home: Welcome(),
+      ),
     );
   }
 }
